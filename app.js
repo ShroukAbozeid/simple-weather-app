5927b0b869d607074680f4331b33b6708b9581bb
Shrouk Abozeid <shroookabozaid@gmail.com>
2021-06-14 23:33:36 +0200

    adding error handling

--- app.js
+++ app.js
@@ -5,23 +5,39 @@ const dataBuffer = fs.readFileSync('api_keys.json')
 const dataJson = dataBuffer.toString();
 const keys = JSON.parse(dataJson)
 
-
+// geocoding request
 const mapUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=" + keys.map_api + '&limit=1'
+console.log(mapUrl)
 request({url: mapUrl, json: true}, (error, response) => {
-    const info = response.body.features[0]
-    const coordinates = info.geometry.center
-    console.log(info.place_name + '. lat is', coordinates[0] , ' long ', coordinates[1]
-    )
+    if (error) {
+        console.log('Unable to connect to gecoding api')
+    } else if (response.body.features.length === 0) {
+        console.log('unable to find location , try another search')
+    }
+    else {
+        const info = response.body.features[0]
+        const coordinates = info.center
+        console.log(info.place_name)
+        console.log('lat :', coordinates[1] , ' long : ', coordinates[0])
+    }
 })
 
+
+// weather request
 const latLng = '37.8267,-122.4233'
 
 const weatherUrl = 'http://api.weatherstack.com/current?access_key=' + keys.weather_api + '&query=' + latLng
 
 request({url: weatherUrl, json: true}, (error, response) => {
-    const info = response.body.current
-    const temp = info.temperature
-    const feelslike = info.feelslike
-    console.log(info.weather_descriptions[0] + '. Temp is', temp , ' but it feels like ', feelslike
-    )
+    if (error) {
+        console.log('Unable to connect to weather api')
+    } else if (response.body.error) {
+        console.log('unable to find location')
+    }
+    else {
+        const info = response.body.current
+        const temp = info.temperature
+        const feelslike = info.feelslike
+        console.log(info.weather_descriptions[0] + '. Temperature is', temp , ' but it feels like ', feelslike)
+    }
 })
